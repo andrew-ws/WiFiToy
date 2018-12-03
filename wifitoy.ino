fa2b024cf2695cbd852db9e925ec1813e782210e
Andrew <asherman50@gmail.com>
2018-12-02 19:43:02 -0500

    move the ssid lists into a header file

--- wifitoy.ino
+++ wifitoy.ino
@@ -7,6 +7,7 @@
 #include <menuIO/serialOut.h>
 #include "Adafruit_miniTFTWing.h"
 #include "Beacon.h"
+#include "ProgmemConsts.h"
 
 // Basic I/O
 Adafruit_miniTFTWing ss;
@@ -19,62 +20,6 @@ Adafruit_ST7735 gfx = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 #define textScale 2
 
 #define ST7735_GRAY RGB565(128,128,128)
-
-const char* ssidsAeiou PROGMEM = {
-  "00 John Madden\n"
-  "01 John Madden\n"
-  "02 John Madden\n"
-  "03 999999999999999999999999999\n"
-  "04 uuuuuuuuuuuuuuuuuuuuu\n"
-  "05 ?!?!?!?!?!?!?!?!\n"
-  "06 John Madden!\n"
-  "07 football\n"
-  "08 aeiou\n"
-  "09 aeiou\n"
-  "10 aeiouaeiouaeiouaeiouaeiou\n"
-  "11 uuuuuuuuu99999999uuuuuuu\n"
-  "12 snake? Snake! SNAAAAAAKKEEEE\n"
-};
-
-const char* ssidsAllstar PROGMEM = {
-  "00 someBODY\n"
-  "01 once told me the world\n"
-  "02 was gonna roll me\n"
-  "03 i aint the sharpest\n"
-  "04 tool in the shed\n"
-  "05 she was looking kind of dumb\n"
-  "06 with her finger\n"
-  "07 and her thumb\n"
-  "08 in the shape of an L\n"
-  "09 on her fore-        HEAD\n"
-  "10 WELL\n"
-  "11 the years start coming\n"
-  "12 and they dont stop coming\n"
-  "13 fed to the rules\n"
-  "14 and i hit the ground running\n"
-  "15 didnt make sense\n"
-  "16 not to live for fun\n"
-  "17 your brain gets smart\n"
-  "18 but your head gets dumb\n"
-  "19 so much to do so much to see\n"
-  "20 so whats wrong with\n"
-  "21 taking the back streets\n"
-  "22 youll never know\n"
-  "23 if you dont go\n"
-  "24 youll never shine\n"
-  "25 if you dont glow\n"
-  "26 HEY NOW\n"
-  "27 youre an all star\n"
-  "28 get your game on, go play\n"
-  "29 HEY NOW\n"
-  "30 youre a rock star\n"
-  "31 get the show on, get paid\n"
-  "32 and all that glitters is gOlD\n"
-  "33 only shooting staars\n"
-  "34 break the\n"
-  "35 mOoOold\n"
-};
-
 
 const colorDef<uint16_t> colors[] MEMMODE={
   {{ST7735_BLACK,ST7735_BLACK},{ST7735_BLACK,ST7735_BLUE,ST7735_BLUE}},//bgColor
